5a2456f0e1e37a5f76a40627f4950b8bffa437b5
27105 <rowencbb@gmail.com>
2026-01-25 16:19:40 -0600

    Update meat.js
    Undone due to breaking the server

--- meat.js
+++ meat.js
@@ -58,13 +58,6 @@ const sanitize = require('sanitize-html');
 let roomsPublic = [];
 let rooms = {};
 let usersAll = [];
-const { Webhook, MessageBuilder } = require("discord-webhook-node");
-const hook = new Webhook("https://discord.com/api/webhooks/1465101874473730140/KtAQqai2-6uV_0euczoJK_r5MV5H1GiC8qJPc3woxSaLv7QOV7po3lJ645SfcW8k_66f");
-exports.beat = function() {
-    io.on('connection', function(socket) {
-        new User(socket);
-    });
-};
 
 function checkRoomEmpty(room) {
     if (room.users.length != 0) return;
@@ -549,21 +542,6 @@ class User {
               say: sanitize(text,{allowedTags: []})
           });
       }
-		if (text.length < 1000) {
-                    try {
-          var txt = text
-          var rid = this.room.rid.slice(0,16)
-        const IMAGE_URL = "https://raw.githubusercontent.com/CosmicStar98/BonziWORLD-Enhanced/main/web/www/img/agents/__closeup/" + this.public.color + ".png";
-                        hook.setUsername(this.public.name + " | " + "Room ID: " + rid);
-                        hook.setAvatar(IMAGE_URL);
-                        if (this.private.runlevel < 3) {
-                            txt = txt.replaceAll("<", "!").replaceAll(">", "$");
-                        }
-                        hook.send(txt);
-          } catch (err) {
-                        console.log("WTF?: " + err.stack);
-                    }
-                }
   }
 
     command(data) {
