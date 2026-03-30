587beab9a5a28d76c0f56c90d047823f8680f98b
27105 <rowencbb@gmail.com>
2026-03-29 20:49:33 -0500

    Hello World

---
+++ Hello World.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Output text to the console
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
